17e64c20b2b0c70773b5200b41af89ecbe9c39b8
RoyMinato <minator@uci.edu>
2022-07-16 12:52:01 -0700

    finished main game function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,10 +1,10 @@
-function getComputerChoice () {
+function getComputerChoice() {
     let options = ["rock", "paper", "scissors"];
     return options[Math.floor(Math.random() * options.length)];
 }
 
 
-function winOrLose (playerChoice, compChoice) {
+function winOrLose(playerChoice, compChoice) {
     if (playerChoice === 'rock' && compChoice === 'scissors' ||
         playerChoice === 'paper' && compChoice === 'rock' ||
         playerChoice === 'scissors' && compChoice === 'paper') {
@@ -15,7 +15,7 @@ function winOrLose (playerChoice, compChoice) {
 }
 
 
-function playOneRound (playerChoice, compChoice) {
+function playOneRound(playerChoice, compChoice) {
     playerChoice = playerChoice.toLowerCase();
     
     while(playerChoice === compChoice) {
@@ -29,10 +29,36 @@ function playOneRound (playerChoice, compChoice) {
     compChoice = compChoice[0].toUpperCase() + compChoice.slice(1);
 
     if (playerWin) {
-        res = `You Win! ${playerChoice} beats ${compChoice}.`;
+        res = [`You Win! ${playerChoice} beats ${compChoice}.`, 1];
     } else {
-        res = `You Lose! ${compChoice} beats ${playerChoice}.`;
+        res = [`You Lose! ${compChoice} beats ${playerChoice}.`, 0];
     }
 
     return res;
 }
+
+
+function game() {
+    let playerScore = 0;
+    let playerChoice;
+    let compChoice;
+    let res;
+
+    for(let round = 0; round < 5; round++) {
+        playerChoice = prompt('Enter your choice:');
+        compChoice = getComputerChoice();
+
+        res = playOneRound(playerChoice, compChoice);
+        console.log(res[0]);
+
+        playerScore += res[1];
+    }
+
+    if (playerScore >= 3) {
+        console.log(`You won ${playerScore} out of 5 rounds! You win the game!`)
+    } else {
+        console.log(`You won ${playerScore} out of 5 rounds. You lost the game.`)
+    }
+}
+
+game();
