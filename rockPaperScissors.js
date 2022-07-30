f6f68596333538b6d0aec432805a1f31f2a6df61
RoyMinato <minator@uci.edu>
2022-07-30 16:07:00 -0700

    Finished logic and basic UI, add style

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,13 +1,24 @@
+let playerScore = 0;
+let compScore = 0;
+let scoreKeep = `Player: ${playerScore}   Computer: ${compScore} \r\n`
+
+function updateScoreKeep (addPlayer, addComp){
+    playerScore += addPlayer;
+    compScore += addComp;
+    scoreKeep = `Player: ${playerScore}   Computer: ${compScore} \r\n`
+}
+
+
 function getComputerChoice() {
-    let options = ["rock", "paper", "scissors"];
+    let options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random() * options.length)];
 }
 
 
 function winOrLose(playerChoice, compChoice) {
-    if (playerChoice === 'rock' && compChoice === 'scissors' ||
-        playerChoice === 'paper' && compChoice === 'rock' ||
-        playerChoice === 'scissors' && compChoice === 'paper') {
+    if (playerChoice === 'Rock' && compChoice === 'Scissors' ||
+        playerChoice === 'Paper' && compChoice === 'Rock' ||
+        playerChoice === 'Scissors' && compChoice === 'Paper') {
             return true;
     } else {
         return false;
@@ -15,55 +26,57 @@ function winOrLose(playerChoice, compChoice) {
 }
 
 
-function playOneRound(playerChoice, compChoice) {
-    playerChoice = playerChoice.toLowerCase();
-    
-    while(playerChoice === compChoice) {
-        compChoice = getComputerChoice();
+//Each game is 5 rounds
+function playRound (e) {
+    if (e.target.nodeName != 'BUTTON' || e.target.textContent === 'Reset Game' ||
+        playerScore >= 5 || compScore >= 5){ 
+        return;
+    }
+
+    playerChoice = e.target.textContent;
+    compChoice = getComputerChoice();
+
+    if(playerChoice === compChoice) {
+        res.textContent = scoreKeep + `It's a draw! Both picked ${playerChoice}.`
+        return;
     }
 
     let playerWin = winOrLose(playerChoice, compChoice);
-    let res;
-
-    playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1);
-    compChoice = compChoice[0].toUpperCase() + compChoice.slice(1);
 
     if (playerWin) {
-        res = [`You Win! ${playerChoice} beats ${compChoice}.`, 1];
+        updateScoreKeep(1,0);
+        res.textContent = scoreKeep + `You Win! ${playerChoice} beats ${compChoice}.`;
     } else {
-        res = [`You Lose! ${compChoice} beats ${playerChoice}.`, 0];
+        updateScoreKeep(0,1);
+        res.textContent = scoreKeep + `You Lose! ${compChoice} beats ${playerChoice}.`;
     }
 
-    return res;
-}
-
-
-function game() {
-    let playerScore = 0;
-    let playerChoice;
-    let compChoice;
-    let res;
-    let options = ["rock", "paper", "scissors"];
-
-    for(let round = 0; round < 5; round++) {
-        playerChoice = prompt('Enter your choice:');
-        compChoice = getComputerChoice();
-
-        while(!options.includes(playerChoice.toLowerCase())) {
-            playerChoice = prompt("Invalid input, please enter 'rock', 'paper', or 'scissors'.");
+    if(playerScore == 5 || compScore == 5){
+        if (playerScore > compScore) {
+            res.textContent = scoreKeep + `You won 5 rounds first! You won the game!`;
         }
-
-        res = playOneRound(playerChoice, compChoice);
-        console.log(res[0]);
-
-        playerScore += res[1];
+        else {
+            res.textContent = scoreKeep + `You lost 5 rounds so you lost the game.`;
+        }
     }
 
-    if (playerScore >= 3) {
-        console.log(`You won ${playerScore} out of 5 rounds! You win the game!`)
-    } else {
-        console.log(`You won ${playerScore} out of 5 rounds. You lost the game.`)
-    }
 }
 
-game();
+
+const res = document.querySelector('#results');
+res.setAttribute('style', 'white-space: pre;')
+res.textContent = scoreKeep + `Make your move!`;
+
+//div containing buttons but only respond to button clicks
+const btns = document.querySelector('#choices'); 
+btns.addEventListener('click', playRound);
+
+const reset = document.querySelector('#reset');
+
+reset.addEventListener('click', () => {
+    playerScore = 0;
+    compScore = 0;
+    updateScoreKeep(-playerScore, -compScore);
+    res.textContent = scoreKeep + `Make your move!`;
+});
+
